a5ff1ad391a7605f881ebc161206becb5a541787
VitalijsKalninsMTU <vitalijs.kalnins@mycit.ie>
2024-12-08 14:47:42 +0000

    quick fix

--- labs/src/main/java/com/example/labs/entities/Household.java
+++ labs/src/main/java/com/example/labs/entities/Household.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -27,6 +28,6 @@ public class Household {
 
     private boolean ownerOccupied;
 
-    @OneToMany(mappedBy = "household", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "household", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Pet> pets;
 }
